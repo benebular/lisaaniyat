ac499f82fef62475f2e31feaf9568b271f8ae8af
Ben Lang <blang@ucsd.edu>
2022-07-15 12:48:27 -0700

    Update dicom_extraction.py

--- dicom_extraction.py
+++ dicom_extraction.py
@@ -2,16 +2,13 @@
 # extracts dicoms from individual folders and puts them in the same folders
 # author: ben lang, blang@ucsd.edu
 
-import sklearn
-import numpy as np
-import pandas as pd
 import os
 import sys
 import time
 import glob
 
 
-source = '/Volumes/circe/rtMRI/lisaaniyat/Sub0271_LP_Vocal_Y0368/Sub0271_dicom'
+source = '/Volumes/hecate/lisaaniyat/Sub0271_LP_Vocal_Y0368/Sub0271_dicom'
 os.chdir(source)
 
 allfolders = glob.glob(os.path.join(source, '*'))
@@ -21,7 +18,7 @@ for folder in allfolders:
     if 'Run' in folder:
         all_folders.append(folder)
 
-destination = '/Volumes/circe/rtMRI/lisaaniyat/Sub0271_LP_Vocal_Y0368/Sub0271_dicom/all_dicom/'
+destination = '/Volumes/hecate/lisaaniyat/Sub0271_LP_Vocal_Y0368/Sub0271_dicom/all_dicom/'
 
 for folder in all_folders:
     source = folder + '/'
